51eeb7dd9b9bb0fa6d693e02ddea7d115a2cc721
Jonah Pearl <jonahpearl@g.harvard.edu>
2025-05-25 13:33:12 -0400

    minor refactors / rearrangements for readability and simplicity

--- tasks/JP/2AFC/shaping/2.00_center_poke_either_side.py
+++ tasks/JP/2AFC/shaping/2.00_center_poke_either_side.py
@@ -13,41 +13,36 @@ center_port = Poke(board.port_4, rising_event="center_poke", falling_event="cent
 final_valve = Digital_output(pin=board.port_1.POW_C)
 
 # State machine
-states = ["wait_for_center_poke", "hold_center_poke", "deliver_odor", "wait_for_side_poke", "left_reward", "right_reward", "inter_trial_interval", "timeout"]
-
-events = ["center_poke", "right_poke", "left_poke", "center_poke_out", "right_poke_out", "left_poke_out", "session_timer"]
-
+states = ["wait_for_center_poke", "deliver_odor", "wait_for_side_poke", "left_reward", "right_reward", "inter_trial_interval", "timeout"]
+events = ["center_poke", "right_poke", "left_poke", "center_poke_out", "right_poke_out", "left_poke_out", "session_timer", "finish_ITI", "center_poke_held"]
 initial_state = "wait_for_center_poke"
 
 # Odor parameters
-pc.v.required_center_hold_duration = 75  # ms
+pc.v.required_center_hold_duration = 150  # ms
 pc.v.odor_delivery_duration = 500
 
 # General Parameters.
 pc.v.session_duration = 1 * pc.hour  # Session duration.
 pc.v.reward_durations = [47, 54]  # Reward delivery duration (ms) [left, right].
-pc.v.reward_duration_multiplier = 1.25
+pc.v.reward_duration_multiplier = 1.0
 pc.v.ITI_duration = 1.5 * pc.second  # Inter trial interval duration.
 pc.v.timeout_duration = 2 * pc.second  # timeout for wrong trials (in addition to ITI)
-pc.v.n_allowed_rwds = 100  # total per session
-
+pc.v.n_allowed_rwds = 125  # total per session
 
 # Variables.
-pc.v.n_rewards = 0  # total number of rewards obtained.
-pc.v.outcome = 0
-pc.v.choice = "right"
 pc.v.entry_time = 0
 pc.v.n_total_trials = 0
+pc.v.mov_ave_correct = 0  # moving avg of last 10 trials
+
+# Reward variables (updated / used in "is_rewarded")
+pc.v.choice = "right"
+pc.v.outcome = 0
 pc.v.n_correct_trials = 0
+pc.v.n_rewards = 0  # total number of rewards obtained.
 pc.v.ave_correct_tracker = pc.OnlineMovingAverage(10)
-pc.v.mov_ave_correct = 0  # moving avg of last ^^ trials
-pc.v.vars_printed = False
+ 
 
-# TODO: during reward licking, in addition to restarting ITI if still licking,
-# if the first thing that happens is a poke OUT and then a poke, they shouldnt be penalized.
-# So poke OUT during wait for center poke should generate an ineligibility period for errors. 
-
-# These funcs are auto-run at beginning + end
+### These funcs are auto-run at beginning + end ###
 def run_start():
     # Set session timer and turn on houslight.
     pc.set_timer("session_timer", pc.v.session_duration)
@@ -62,18 +57,33 @@ def run_end():
     pass
 
 
+### Helper funcs ###
 def is_rewarded(side):
     ### Always rewarded in this task
     pc.v.choice = side
     pc.v.outcome = 1
     pc.v.n_correct_trials += 1
+    pc.v.n_rewards += 1  # one reward per trial in this task
     pc.v.ave_correct_tracker.add(1)
     return pc.v.outcome
 
-### State behaviour functions ###
+
+# State-independent behaviour.
+def all_states(event):
+    # When 'session_timer' event occurs stop framework to end session.
+    if event == "session_timer":
+        pc.stop_framework()
+    if pc.v.n_rewards >= pc.v.n_allowed_rwds:
+        pc.stop_framework()
+
+
+### State-machine ###
 
 def wait_for_center_poke(event):
+    # NB: this state can occur multiple times in a row.
+    # Use the inter_trial_interval state for once-per-trial updates.
 
+    # Cue mouse that trial is available
     if event == "entry":
         center_port.LED.on()
         pc.v.entry_time = pc.get_current_time()  # Start early-error buffer
@@ -92,17 +102,14 @@ def wait_for_center_poke(event):
     elif (event == "left_poke_out" or event == "right_poke_out"):
         pc.v.entry_time = pc.get_current_time()
 
+    # Require mouse to hold nose in center port for a certain amt of time.
+    # If it does not, it's not an error, just nothing happens.
     elif event == "center_poke":
-        pc.goto_state("hold_center_poke")
-
-
-# Require mouse to hold nose in center port for a certain amt of time
-# If not, it's not an error, just nothing happens.
-def hold_center_poke(event):
-    if event == "entry":
-        pc.timed_goto_state("deliver_odor", pc.v.required_center_hold_duration)
+        pc.set_timer("center_poke_held", pc.v.required_center_hold_duration)
     elif event == "center_poke_out":
-        pc.goto_state("wait_for_center_poke")  # cancels the timed transition
+        pc.disarm_timer("center_poke_held")
+    elif event == "center_poke_held":
+        pc.goto_state("deliver_odor")
 
 
 # Just air in this shaping task
@@ -122,12 +129,10 @@ def wait_for_side_poke(event):
     elif event == "right_poke":
         if is_rewarded("right"):
             pc.goto_state("right_reward")
-            pc.v.n_rewards += 1
 
     elif event == "left_poke":
         if is_rewarded("left"):
             pc.goto_state("left_reward")
-            pc.v.n_rewards += 1
 
 
 def left_reward(event):
@@ -147,6 +152,7 @@ def right_reward(event):
     elif event == "exit":
         right_port.SOL.off()
 
+
 def timeout(event):
     if event == "entry":
         pc.v.ave_correct_tracker.add(0)
@@ -154,31 +160,38 @@ def timeout(event):
 
 
 def inter_trial_interval(event):
-    # Go to init trial after specified delay.
     if event == "entry":
-        pc.timed_goto_state("wait_for_center_poke", pc.v.ITI_duration)
-        pc.v.n_total_trials += 1
+         
+        # Start ITI timer. Using a timer instead of "timed_goto_state()"
+        # allows us to reset the timer if mouse isn't finished licking 
+        # the reward, without having to restart the entire ITI state, 
+        # which would require lots of flags to only update things once, 
+        # and would be generally confusing.
+        pc.set_timer("finish_ITI", pc.v.ITI_duration)
         pc.v.entry_time = pc.get_current_time()
+
+        # Update vars
         pc.v.mov_ave_correct = pc.v.ave_correct_tracker.ave
-        if not pc.v.vars_printed:
-            # prevent vars from being printed every time mouse licks in ITI...
-            pc.print_variables(["n_total_trials", "n_correct_trials", "mov_ave_correct", "required_center_hold_duration"])
-            pc.v.vars_printed = True
+        pc.v.n_total_trials += 1
+        pc.print_variables(["n_total_trials", "n_correct_trials", "mov_ave_correct", "required_center_hold_duration"])
+        
+        # Auto-increase center hold duration for shaping
+        if (
+            ((pc.v.n_rewards == 25) or (pc.v.n_rewards == 50))
+            and (pc.v.required_center_hold_duration < 300)
+        ):
+            pc.v.required_center_hold_duration += 75
+    
+    # If mouse is still licking the reward, let it keep going until it's done.
     elif (
-        ((event == "left_poke") or (event == "right_poke"))
+        pc.v.outcome
+        and (
+                ((event == "left_poke") and pc.v.choice == "left")
+                or ((event == "right_poke") and pc.v.choice == "right")
+            )
         and ((pc.get_current_time() - pc.v.entry_time) < (pc.v.ITI_duration/2))
     ):
-        # If mouse is still licking the reward, let it keep going until it's done.
-        # TODO: only do this on correct trials and for the correct port!
-        pc.goto_state("inter_trial_interval")
-    elif event == "exit":
-        pc.v.vars_printed = False
-
-
-# State independent behaviour.
-def all_states(event):
-    # When 'session_timer' event occurs stop framework to end session.
-    if event == "session_timer":
-        pc.stop_framework()
-    if pc.v.n_rewards >= pc.v.n_allowed_rwds:
-        pc.stop_framework()
+        pc.reset_timer("finish_ITI", pc.v.ITI_duration)
+    
+    elif event == "finish_ITI":
+        pc.goto_state("wait_for_center_poke")
